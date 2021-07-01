5bddb754a534e353429c797c192a22ccb1ea5b01
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-07-01 15:34:30 +0300

    01.07.2021 15.34

---
+++ 1. Gün/printFonksiyonu.py
@@ -0,0 +1,8 @@
+# Python’da # işareti kullanıldığı satırı yorum satırı yapar. Ayrıca """ """" arasına yazılacak yazıların tamamı yorum satırı olacaktır.
+# Yorum satırları kod bloklarında okunmaz. Sadece yazılımcıların yaptıklarını unutmamak ve eğer projede birden fazla çalışan varsa yaptıklarını anlarlar. Bilgilendirme satırlarıdır. 
+#print fonksiyonu ekrana yazdırmayı sağlar.
+#Print fonksiyonu kullanılırken yazılacak ifade () içine yazılır. Yani ifademiz şöyle olur print() 
+#String ifadeler tek tırnak, çift tırnak ya da üç tırnak içinde yazılır.
+#Sayısal ifadeleri direkt olarak parantez içinde yazılabilir.
+print("Merhaba Dünya") #Merhaba Dünya çıktısı verir.
+print(2) #2 çıktısını verecektir.
---
+++ 1. Gün/veriTurleri.py
@@ -0,0 +1,35 @@
+#VERİ TÜRLERİ
+"""
+1.STRING VERİ TÜRÜ
+*Metinsel ifadeleri içerir. Harfler, kelimeler, cümleler, metinler ve paragraflar bu gruba girer.
+*Bir ifade tırnak ifadesi içine yazılmışsa string olarak değerlendirilir. Çift tırnak ya da tek tırnak olması önemli değildir.
+*Eğer tek tırnak ile başladıysa tek tırnak, çift tırnak ile başladıysa çift tırnakla biter.
+*String ifadeler + operatörü ile birleştirme işlemi yapılabilir.
+*String ifadelerde çarpı koyarsanız ekrana o kadar o string ifadeyi bastırır.
+"""
+print("Merhaba")
+print('Python öğreniyorum.')
+print("Ben bir BÖTE öğrencisiyim" + " ama yazılımcı olmak istiyorum.")
+print("python "*10)
+
+
+""" 
+2. Sayısal Veri Türleri 
+a.integer
+*Tam sayıları ifade eder. Mesela 5,12, 0 ,22, -431...
+b.float veri türü
+Ondalıklı sayıları ifade eder. 5.3  7.8 0.0 -14.2 ....
+"""
+print(5)
+print(40)
+print(-40)
+
+print(4.2)
+print(0.0)
+print(-14.2)
+
+""" 
+3.boolean Veri Türü
+*İki değeri vardır ya true(1) ya da false(0) değerini dönderir.
+*Genellikle mantıksal ifadelerde yer verilir.
+"""
